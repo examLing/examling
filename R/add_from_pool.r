800845b245f148331ceddb5a66e18691d4292c96
Brighton Pauli <brightp@umich.edu>
2022-08-22 18:40:16 -0400

    Added first string question templates.

---
+++ R/add_from_pool.r
@@ -0,0 +1,29 @@
+## add_from_pool.r
+
+#' Add a question to a dataframe from a pool of answers.
+#'
+#' @param question Question text.
+#' @param image Image filename (optional).
+#' @param explanation Explanation for the correct answer (optional).
+#' @param answer_pool Tibble of answer choices, with id and text columns.
+#' @param correct_ids Numeric vector of correct answer ids, as in answer_pool.
+#' @param df Dataframe to add question to.
+#'
+#' If a dataframe is not provided, a new one is created.
+#'
+#' @details #Credits
+#' Brighton Pauli, 2022.
+#'
+#' @export
+
+add_from_pool <- function(question, image = NA, explanation = NA,
+    answer_pool = NA, correct_ids = NA, df = NA) {
+    ## generate correct and incorrect
+    indices <- answer_pool$id %in% correct_ids
+    correct <- answer_pool$text[indices]
+    incorrect <- answer_pool$text[!indices]
+
+    ## wrap around the other add_question function
+    add_question(question, image = image, explanation = explanation,
+        correct = correct, incorrect = incorrect, df = df)
+}
--- R/add_question.r
+++ R/add_question.r
@@ -5,15 +5,9 @@
 #' @param question Question text.
 #' @param image Image filename (optional).
 #' @param explanation Explanation for the correct answer (optional).
-#' @param correct_ids Numeric vector of correct answer indices.
-#' @param choices Tibble of answer choices, with id and text columns.
 #' @param correct Vector of possible correct answers.
 #' @param incorrect Vector of possible incorrect answers.
 #' @param df Dataframe to add question to.
-#'
-#' If a dataframe is not provided, a new one is created.
-#'
-#' ONLY correct_ids+choices OR correct+incorrect should be provided.
 #'
 #' If you use the first pair, "correct" and "incorrect" vectors will be
 #' generated automatically from the choices using the given indices.
@@ -23,26 +17,8 @@
 #'
 #' @export
 
-add_question <- function(question, image = NA, explanation = NA,
-    correct_ids = NA, choices = NA, correct = NA, incorrect = NA, df = NA) {
-    ## correct_ids+choices OR correct+incorrect must be given, but not both
-    if (is.na(correct_ids) != is.na(choices)) {
-        stop("correct_ids and choices must be given together")
-    }
-    if (is.na(correct) != is.na(incorrect)) {
-        stop("correct and incorrect must be given together")
-    }
-    if (is.na(correct_ids) == is.na(correct)) {
-        stop("either correct_ids or correct must be used, but not both")
-    }
-
-    ## if correct_ids and choices are given, generate correct and incorrect
-    if (!is.na(correct_ids) && !is.na(choices)) {
-        indices <- choices$id %in% correct_ids
-        correct <- choices$text[indices]
-        incorrect <- choices$text[!indices]
-    }
-
+add_question <- function(question, image = NA, explanation = NA, correct = NA,
+    incorrect = NA, df = NA) {
     ## build dataframe
     ndf <- data.frame(question = question, image = image,
         correct = I(list(correct)), incorrect = I(list(incorrect)),
