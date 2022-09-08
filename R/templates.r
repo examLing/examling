4d8e8f13248efaa6cee112392dce621838b4fc97
Brighton Pauli <brightp@umich.edu>
2022-09-08 15:12:44 -0400

    Finished version 1 of exams2js

--- R/templates.r
+++ R/templates.r
@@ -6,6 +6,30 @@ date: \"`r Sys.Date()`\"
 %s---
 "
 
+js_submit <- "<p>
+    <button id=\"sub%qid\">Submit</button>
+</p>
+<p id=\"res%qid\">
+    Click to submit.
+</p>
+<script>
+const btn%qid = document.querySelector('#sub%qid')
+btn%qid.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name=\"q%qid\"]:checked');
+    let value = 0;
+    checkboxes.forEach((checkbox) => {
+        value += 1 << checkbox.value;
+    });
+    console.log(value);
+    if (value == %correct) {
+        document.getElementById(\"res%qid\").innerHTML = \"Correct\";
+    } else {
+        document.getElementById(\"res%qid\").innerHTML = \"Incorrect\";
+    }
+});
+</script>
+"
+
 schoice <- "
 Question
 ========
