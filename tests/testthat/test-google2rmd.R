0a1361f38acd55adf7f80f60a0461ef3332fcdec
Brighton Pauli <brightp@umich.edu>
2023-08-04 14:56:14 -0400

    Set up actual testing for google2rmd

---
+++ tests/testthat/test-google2rmd.R
@@ -0,0 +1,42 @@
+test_that("Load sheet by name", {
+    unlink("Example/*")
+    url <- paste0(
+        "docs.google.com/spreadsheets/d/",
+        "1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
+        collapse = ""
+    )
+
+    google2rmd(
+        url,
+        "Example",
+        sheet = "Truth Conditions"
+    )
+
+    google2rmd(
+        url,
+        "Example",
+        sheet = "Lambda Calculus"
+    )
+
+    expect_gt(length(list.files("Example")), 0)
+})
+
+filenames <- list.files("Example")
+
+for (filename in filenames) {
+    expectname <- paste0("ExpectedRmd/", filename)
+    actualname <- paste0("Example/", filename)
+    if (file.exists(expectname)) {
+        test_that(filename, {
+            expecttext <- expectname %>%
+                read_file %>%
+                gsub("\r", "", .)
+
+            actualtext <- actualname %>%
+                read_file %>%
+                gsub("\r", "", .)
+
+            expect_equal(actualtext, expecttext)
+        })
+    }
+}
