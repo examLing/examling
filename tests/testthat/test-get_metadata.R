86a7e1dfdb59530f7f4928a5aedf1bd7d733aea7
Brighton Pauli <brightp@umich.edu>
2023-08-04 14:19:38 -0400

    Tested get_metadata

--- tests/testthat/test-get_metadata.R
+++ tests/testthat/test-get_metadata.R
@@ -12,7 +12,46 @@ test_that("Find all metadata in Types1.Rmd", {
     expect_equal(metadata, expected)
 })
 
-# test_that("Find all metadata in Types1", {
-#     get_metadata("Types1", "Example")
-#     expect_true(true)
-# })
+test_that("Find all metadata in Types1, with `paste`d values", {
+    expected <- list(
+        difficulty = 1,
+        part = 1,
+        source = "Kearns",
+        chapter = 4,
+        is_dynamic = TRUE
+    )
+
+    expected <- lapply(expected, paste)
+
+    metadata <- get_metadata("Types1", "Example")
+
+    expect_equal(metadata, expected)
+})
+
+test_that("Find all metadata in Types1.Rmd, wrong extension", {
+    expected <- list(
+        difficulty = "1",
+        part = "1",
+        source = "Kearns",
+        chapter = "4",
+        is_dynamic = "TRUE"
+    )
+
+    metadata <- get_metadata("Types1.txt", "Example")
+
+    expect_equal(metadata, expected)
+})
+
+test_that("Find all metadata in Types1.Rmd, no extension", {
+    expected <- list(
+        difficulty = "1",
+        part = "1",
+        source = "Kearns",
+        chapter = "4",
+        is_dynamic = "TRUE"
+    )
+
+    metadata <- get_metadata("Types1.", "Example")
+
+    expect_equal(metadata, expected)
+})
