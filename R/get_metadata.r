4acc4f82068bdf0329843093f68512e04ae02cc8
brightp-py <brightp@umich.edu>
2023-08-20 14:29:16 -0400

    Getting metadata now returns a tibble with dynamic metadata

--- R/get_metadata.r
+++ R/get_metadata.r
@@ -27,36 +27,95 @@ get_metadata <- function(filename = "", directory = "") {
     }
 
     filename <- gsub("(\\.[^.]*)?$", ".Rmd", filename)
-
     text <- read_file(filename)
-    metadata_title <- regexpr("Meta-information\r?\n=+", text)
-    text <- substring(text, metadata_title)
 
-    metadata <- list()
+    metadefault <- get_default_metadata_(text)
+    metadefault$filename <- filename
+    metadefault$qvariation <- 0
+
+    metadata <- tibble()
+    cut_text <- text
 
     for (i in seq_len(1000)) {
-        match <- regexpr("exextra\\[[^\r\n]+\\]", text)
+        match <- regexpr("(add(_string)?_question)|(add_from_pool)", cut_text)
+        if (match == -1) break
+
+        vari_text <- substring(cut_text, 1, match)
+        cut_text <- substring(cut_text, match + attr(match, "match.length"))
+
+        next_row <- metadefault %>%
+            get_dynamic_metadata_(vari_text, i)
+
+        too_long <- which(lapply(next_row, length) > 1)
+        next_row[too_long] <- next_row[too_long] %>%
+            lapply(paste0, collapse = "\n")
+
+        metadata[which(!names(next_row) %in% names(metadata))] <- NA
+        metadata <- rbind(metadata, next_row)
+    }
+
+    # browser()
+
+    metadata <- relocate(metadata, "filename", "qvariation")
+
+    metadata
+}
+
+get_default_metadata_ <- function(text) {
+    metadata_title <- regexpr("Meta-information\r?\n=+", text)
+    cut_text <- substring(text, metadata_title)
+
+    metadefault <- list()
+
+    for (i in seq_len(1000)) {
+        match <- regexpr("exextra\\[[^\r\n]+\\]", cut_text)
         if (match == -1) break
 
         name <- substring(
-            text,
+            cut_text,
             match + 8,
             match + attr(match, "match.length") - 2
         )
-        text <- substring(text, match + 8)
+        cut_text <- substring(cut_text, match + 8)
 
-        val_match <- text %>%
-            substring(1, regexpr("\r?\n", text)) %>%
+        val_match <- cut_text %>%
+            substring(1, regexpr("\r?\n", cut_text)) %>%
             regexpr(": [^\r\n]+", .)
 
         value <- substring(
-            text,
+            cut_text,
             val_match + 2,
             val_match + attr(val_match, "match.length") - 1
         )
 
-        metadata[[name]] <- c(metadata[[name]], value)
+        metadefault[[name]] <- c(metadefault[[name]], value)
     }
 
-    metadata
+    metadefault
+}
+
+get_dynamic_metadata_ <- function(metadefault, text, i) {
+    match <- regexpr(r"(# VARIATION ?\d*)", text)
+    if (match == -1) return(metadefault)
+
+    res <- metadefault
+    res$qvariation <- i
+    cut_text <- substring(text, match)
+
+    for (j in seq_len(1000)) {
+        match <- regexpr("# [^\r\n]+: [^\r\n]+", cut_text)
+        if (match[[1]] == -1) break
+
+        data <- cut_text %>%
+            substring(
+                match + 2,
+                match + attr(match, "match.length") - 1
+            ) %>%
+            strsplit(": ") %>%
+            unlist()
+        
+        res[[data[[1]]]] <- data[[2]]
+    }
+
+    res
 }
--- tests/testthat/test-get_metadata.R
+++ tests/testthat/test-get_metadata.R
@@ -1,41 +1,43 @@
 test_that("Find all metadata in Types1.Rmd", {
-    expected <- list(
-        source = "Kearns",
-        chapter = "4",
-        url = "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
-        sheet = "Lambda Calculus",
-        is_dynamic = "TRUE"
-    )
+    expected <- tribble(
+        ~filename, ~qvariation, ~source, ~chapter, ~url, ~sheet, ~is_dynamic, ~difficulty,
+        "ExpectedRmd/Types1.Rmd", 1, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 2, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 3, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 4, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 5, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 6, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 7, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 8, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 9, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 10, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 11, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 12, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 13, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2"
+    ) %>% data.frame()
 
     metadata <- get_metadata("Types1.Rmd", "ExpectedRmd")
 
     expect_equal(metadata, expected)
 })
 
-test_that("Find all metadata in Types1, with `paste`d values", {
-    expected <- list(
-        source = "Kearns",
-        chapter = 4,
-        url = "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
-        sheet = "Lambda Calculus",
-        is_dynamic = TRUE
-    )
-
-    expected <- lapply(expected, paste)
-
-    metadata <- get_metadata("Types1", "ExpectedRmd")
-
-    expect_equal(metadata, expected)
-})
-
 test_that("Find all metadata in Types1.Rmd, wrong extension", {
-    expected <- list(
-        source = "Kearns",
-        chapter = "4",
-        url = "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
-        sheet = "Lambda Calculus",
-        is_dynamic = "TRUE"
-    )
+    expected <- tribble(
+        ~filename, ~qvariation, ~source, ~chapter, ~url, ~sheet, ~is_dynamic, ~difficulty,
+        "ExpectedRmd/Types1.Rmd", 1, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 2, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 3, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 4, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 5, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 6, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 7, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 8, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 9, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 10, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 11, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 12, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 13, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2"
+    ) %>% data.frame()
 
     metadata <- get_metadata("Types1.txt", "ExpectedRmd")
 
@@ -43,13 +45,22 @@ test_that("Find all metadata in Types1.Rmd, wrong extension", {
 })
 
 test_that("Find all metadata in Types1.Rmd, no extension", {
-    expected <- list(
-        source = "Kearns",
-        chapter = "4",
-        url = "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
-        sheet = "Lambda Calculus",
-        is_dynamic = "TRUE"
-    )
+    expected <- tribble(
+        ~filename, ~qvariation, ~source, ~chapter, ~url, ~sheet, ~is_dynamic, ~difficulty,
+        "ExpectedRmd/Types1.Rmd", 1, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 2, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 3, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 4, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 5, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 6, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "1",
+        "ExpectedRmd/Types1.Rmd", 7, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 8, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 9, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 10, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 11, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 12, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2",
+        "ExpectedRmd/Types1.Rmd", 13, "Kearns", "4", "docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Lambda Calculus", "TRUE", "2"
+    ) %>% data.frame()
 
     metadata <- get_metadata("Types1.", "ExpectedRmd")
 
@@ -57,14 +68,23 @@ test_that("Find all metadata in Types1.Rmd, no extension", {
 })
 
 test_that("Metadata with multi-line issue", {
-    expected <- list(
-        source = "Allwood",
-        chapter = "2",
-        issue = c("no difficulty", "Code block in string."),
-        url = "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
-        sheet = "Truth Conditions",
-        is_dynamic = "TRUE"
-    )
+    # expected <- list(
+    #     source = "Allwood",
+    #     chapter = "2",
+    #     issue = c("no difficulty", "Code block in string."),
+    #     url = "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4",
+    #     sheet = "Truth Conditions",
+    #     is_dynamic = "TRUE"
+    # )
+    expected <- tribble(
+        ~filename, ~qvariation, ~source, ~chapter, ~issue, ~url, ~sheet, ~is_dynamic,
+        "ExpectedRmd/Settheory16.Rmd", 1, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE",
+        "ExpectedRmd/Settheory16.Rmd", 2, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE",
+        "ExpectedRmd/Settheory16.Rmd", 3, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE",
+        "ExpectedRmd/Settheory16.Rmd", 4, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE",
+        "ExpectedRmd/Settheory16.Rmd", 5, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE",
+        "ExpectedRmd/Settheory16.Rmd", 6, "Allwood", "2", "no difficulty\nCode block in string.", "https://docs.google.com/spreadsheets/d/1r3cH6oVKfEm6OiYj8uSk4YidVJs4a-xgSF5-w3iBbQ4", "Truth Conditions", "TRUE"
+    ) %>% data.frame()
 
     metadata <- get_metadata("Settheory16.Rmd", "ExpectedRmd")
 
